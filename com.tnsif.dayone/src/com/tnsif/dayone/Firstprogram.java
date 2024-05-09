5a666c91e62ae275fcb87a2e899ea278ec54543e
User <User@DESKTOP-AM314LL>
2024-05-09 22:43:43 +0530

    commit 2

--- com.tnsif.dayone/src/com/tnsif/dayone/Firstprogram.java
+++ com.tnsif.dayone/src/com/tnsif/dayone/Firstprogram.java
@@ -3,7 +3,7 @@ package com.tnsif.dayone;
 public class Firstprogram {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("welcome to github");
 
 	}
 
